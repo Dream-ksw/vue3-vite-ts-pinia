0b94bf3652843eac77bea66f036f7eef5416cb83
Dream-v10 <85750637+Dream-v10@users.noreply.github.com>
2023-02-07 19:10:50 +0800

    复习compositionapi以及认识render函数

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp, toRaw } from 'vue'
-// toRaw 转为原始对象
+// import App from './App.vue'
 import App from './App.vue'
 import './assets/base.less'
+// 注册loading插件
+// import Loading from './components/Loading/index'
 import Card from './components/Card/Card.vue'
 // 引入element
 import ElementPlus from 'element-plus'
@@ -10,43 +12,42 @@ import 'element-plus/dist/index.css'
 import 'animate.css'
 // 引入unocss 设置全局css类名
 import 'uno.css'
-
 // 引入pinia
 import { createPinia, PiniaPluginContext } from 'pinia'
+
 // pinia持久化
-type Options = {
-  key?: string
-}
-
-const __piniaKey__:string = 'xiaoman'
-
-const setStorage = (key: string, value: any) => {
-  localStorage.setItem(key, JSON.stringify(value))
-}
-const getStorage = (key:string) => {
-  return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key) as string) : {}
-}
-const piniaPlugin = (options: Options) => {
-  return (context: PiniaPluginContext) => {
-    const {store} = context
-    const data = getStorage(`${options?.key ?? __piniaKey__}-${store.$id}`)
-    store.$subscribe(() => {
-      setStorage(`${options?.key ?? __piniaKey__}-${store.$id}`, toRaw(store.$state))
-    })
-    // 注意这里先获取data 或者 在监听之后获取data没有影响
-    // 因为当修改数据时 store中对页面中的数据进行了修改 此时不会使用这个data  只有当页面刷新的时候才会使用这里data
-    return data
-  }
-}
-
+// type Options = {
+//   key?: string
+// }
+// const __piniaKey__:string = 'xiaoman'
+// const setStorage = (key: string, value: any) => {
+//   localStorage.setItem(key, JSON.stringify(value))
+// }
+// const getStorage = (key:string) => {
+//   return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key) as string) : {}
+// }
+// const piniaPlugin = (options: Options) => {
+//   return (context: PiniaPluginContext) => {
+//     const {store} = context
+//     const data = getStorage(`${options?.key ?? __piniaKey__}-${store.$id}`)
+//     store.$subscribe(() => {
+//       setStorage(`${options?.key ?? __piniaKey__}-${store.$id}`, toRaw(store.$state))
+//     })
+//     // 注意这里先获取data 或者 在监听之后获取data没有影响
+//     // 因为当修改数据时 store中对页面中的数据进行了修改 此时不会使用这个data  只有当页面刷新的时候才会使用这里data
+//     return data
+//   }
+// }
 const store = createPinia()
-store.use(piniaPlugin({
-  key: 'pinia'
-}))
+// store.use(piniaPlugin({
+//   key: 'pinia'
+// }))
 
 // (1)全局引入mitt 兄弟间组件传值
 // import mitt from 'mitt'
 // const Mit = mitt()
+// app.config.globalProperties.$Bus = Mit
+
 // ts声明文件
 // declare module 'vue' {
 //   export interface ComponentCustomProperties {
@@ -54,8 +55,7 @@ store.use(piniaPlugin({
 //   }
 // }
 
-// 注册loading插件
-import Loading from './components/Loading/index'
+
 
 const app = createApp(App)
 
@@ -77,20 +77,20 @@ const app = createApp(App)
 //   }
 // }
 
-app.use(Loading)
-type Loading = {
-  show: () => void,
-  hide: () => void
-}
-declare module 'vue' {
-  export interface ComponentCustomProperties {
-    $Loading: Loading
-  }
-}
+
+// type Loading = {
+//   show: () => void,
+//   hide: () => void
+// }
+// declare module 'vue' {
+//   export interface ComponentCustomProperties {
+//     $Loading: Loading
+//   }
+// }
+// app.use(Loading)
 
 // 全局组件
 app.component('Card', Card)
 app.use(ElementPlus)
 app.use(store)
-// app.config.globalProperties.$Bus = Mit
 app.mount('#app')
---
+++ src/practice/901_compositionapi练习.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <!-- {{ people.name }}
+    <button @click="change">修改</button> -->
+
+    <!-- {{ name }} - {{ age }}
+    <div>
+      <button @click="changeName">修改name</button>
+      <button @click="changeAge">修改age</button>
+    </div> -->
+
+    <!-- <h2 ref="h2El">123</h2> -->
+
+    <!-- {{ info.name }}
+    <button @click="info.name = 'abc'">修改</button> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, toRef, shallowRef, triggerRef, watchEffect, watch, readonly } from 'vue'
+
+// reactive 返回一个对象的响应式代理
+// const people = reactive<{name: string, age: number}>({
+//   name: 'why',
+//   age: 18
+// })
+
+// toRefs: 将reactive对象中所有属性全部转换为ref对象
+// toRef: 将reactive对象中某个属性转换为ref对象
+// let {name, age} = toRefs(people)
+// let age = toRef(people, 'age')
+
+// shallowRef 浅层ref 深层的数据不具有响应式  使用triggerRef()触发视图更新
+// const people = ref({name: 'k'})
+// const people = shallowRef({name: 'k'})
+// const change = () => {
+//   people.value.name = 'kkk'
+//   triggerRef(people)
+// }
+
+// const name = ref('coder')
+// const age = ref(15)
+// const changeName = () => name.value = 'coderwhy'
+// const changeAge = () => age.value++
+
+// watchEffect组件创建会立即执行一次 收集函数中用到的可响应式数据 并添加到依赖中  只要依赖发生变化watchEffect就会执行
+// watchEffect((onCleanup) => {
+//   let timer:any = null
+//   timer = setTimeout(() => {
+//     console.log('网络请求成功')
+//   }, 1000);
+//   onCleanup(() => {
+//     // 清除上一次的请求
+//     clearTimeout(timer)
+//   })
+//   console.log(age.value);
+// })
+
+// const h2El = ref(null)
+// onMounted(() => {
+//   console.log(h2El.value)
+// })
+// watchEffect(() => {
+//   console.log(h2El.value);
+// },{
+//   flush: 'post',
+//   // pre 元素挂载或者更新之前执行
+//   // post 使侦听器延迟到组件渲染之后再执行
+//   // sync  强制效果始终同步触发
+// }
+// )
+
+// const info = reactive({name: 'coder', age: 18})
+// watch(info, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)  // proxy对象
+// })
+// watch(() => ({...info}), (newValue, oldValue) => {
+//   console.log(newValue, oldValue) // 普通对象
+// })
+// const name = ref('coder')
+// watch(name, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)
+// })
+// watch(() => {}, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)
+// })
+
+
+</script>
+
+<style scoped>
+
+</style>
